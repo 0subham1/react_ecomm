ac3c056ba831b8486535815fd9428dfb5a0c52df
Subham Gupta <99577900+0subham1@users.noreply.github.com>
2023-01-30 08:29:15 +0530

    live url

--- src/Const.js
+++ src/Const.js
@@ -1,4 +1,4 @@
 // export const BASE_URL_INVENTORY = "https://dev.pyramidcafe.com:8443/InventoryService/"
 
-export const BASE_URL="http://localhost:4000/"
-// export const BASE_URL="https://e-comm-server2.onrender.com/"
+// export const BASE_URL="http://localhost:4000/"
+export const BASE_URL="https://server3-he38ittfs-0subham1.vercel.app/"
